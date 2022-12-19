6b5cd4b8a44ca1008a8a91269e213c1531451216
SorogS <esorogovets@mail.ru>
2022-12-19 22:55:11 +0300

    add 8 lab

---
+++ Planes/MilitaryPlane.js
@@ -0,0 +1,12 @@
+const Plane = require('./Plane');
+
+class MilitaryPlane extends Plane {
+
+    constructor(model, maxSpeed, maxFlightDistance, maxLoadCapacity, militaryType) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.militaryType = militaryType;
+
+    }
+}
+
+module.exports = MilitaryPlane;
